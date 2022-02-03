8c94d74ed50957a531f0dc62f519b52926c94d19
A.Rone <75527849+Roney-a@users.noreply.github.com>
2022-02-03 19:55:11 +0900

    Add files via upload

---
+++ Presen/script.js
@@ -0,0 +1,20 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+const pages = [
+  "./DIG_プレゼン/スライド1.JPG",
+  "./DIG_プレゼン/スライド2.JPG",
+  "./DIG_プレゼン/スライド3.JPG",
+  "./DIG_プレゼン/スライド4.JPG",
+  "./DIG_プレゼン/スライド5.JPG",
+  "./DIG_プレゼン/スライド6.JPG",
+  "./DIG_プレゼン/スライド7.JPG",
+  "./DIG_プレゼン/スライド8.JPG",
+  "./DIG_プレゼン/スライド9.JPG",
+  "./DIG_プレゼン/スライド10.JPG",
+  "./DIG_プレゼン/スライド11.JPG",
+  "./DIG_プレゼン/スライド12.JPG"
+];
+
+let slide = document.getElementsByTagName("img");
+console.log(slide);
